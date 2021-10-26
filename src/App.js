36bc1936d5c4b4891890a8b9175eb85cf13e80b7
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-10-26 01:12:10 -0400

    adding the components and organizing the appjs file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from "react"
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+import Main from "./components/Main"
+import Footer from "./components/Footer/Footer"
+import Nav from "./components/Nav/Nav"
+
 
 function App() {
   return (
     <div className="App">
-      <h2>Yurrr this is the wedding page</h2>
+      
+      <BrowserRouter>
+<Nav/>
+        <Route path='/' exact={true} component={Main}/>   
+      </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Nav() {
+    return (
+      <div>
+        <h2>This is my Nav bar section</h2>
+      </div>
+    );
+  }
+  
+  export default Nav;
