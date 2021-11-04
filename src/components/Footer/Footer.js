8518292ebce29472fafd4878a8a9edf7aa925187
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-11-03 23:46:34 -0400

    cssing the webpage

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,79 +1,33 @@
 import React from "react"
 import "./style.css"
 import Ig from "./instagram.png"
-import Github from "./gitH.png"
 import Phone from "./phone.png"
 import Email from "./mailE.png"
-import Linkedin from "./linkedIn.png"
 import Twitter from "./twittwe.png"
 
 
-const footerBg={
-  backgroundColor:'white',
-  height:'200px',
-  display:'flex',
-  justifyContent:'center',
-  flexDirection:'column'
-}
-const footerInfo={
- color:'#222222',
- margin:'30px',
- fontSize:'15px',
- fontWeight:'bold',
- fontFamily: 'Encode Sans SC, sans-serif',
- textDecoration:'none'
-}
-const letsTalk={
-  textDecoration:'none',
-  color:'red',
-  fontSize:'50px',
-  color:'#222222'
-}
+
 const footerImg ={
-  width: 20,
+  width: 30,
   height: 30,
   borderRadius:'100%',
   margin: '12px',
   }
 
-
 function Footer() {
     return (
       <>
-         <div className='myFooter'>
-
-      
-<nav style={footerBg} className="navbar navbar-expand  ">
-
-<div class='questionAsk'> 
-  <h1>Looking for a Front-End Developer? 
-    <br/>
-    <br/>
-   & a Back-End Developer?</h1>
-  <a style={letsTalk}  href="mailto:kevinrhidalgo@yahoo.com"><h3 >Let's talk</h3> </a> 
+        
+<div className="footer">
+  <h1>You can find us at</h1>
+  <div className="theContacts">
+  <a href="instagram.com"><img style={footerImg} src={Ig}/></a> 
+  <a href="tel:646-285-5696"><img style={footerImg} src={Phone}/></a>
+  <a href="mailto:kevinrhidalgo@yahoo.com"><img style={footerImg} src={Email}/></a>
+  <a href="twitter.com"><img style={footerImg} src={Twitter}/></a>
   </div>
 
-  
-  <div className="footerInfo">
-  <p style={footerInfo}><a style={footerInfo} href="mailto:kevinrhidalgo@yahoo.com">Collaborate with Me</a></p>
-
-  <p style={footerInfo}> © 2021. Kevin Hidalgo Portfolio</p>
-
-  <p style={footerInfo}>Contacts</p>
-
-    <h1 style={footerInfo} >Resume</h1>
-
-</div>
-
-<div className="contactList">
-   <a href="https://www.instagram.com/"><img style={footerImg} src={Ig}/></a> 
-   <a href="https://github.com//kevinrhidalgo"><img style={footerImg} src={Github}/></a> 
-    <a href="tel:646-285-5696"><img style={footerImg} src={Phone}/></a>
-    <a href="mailto:kevinrhidalgo@yahoo.com"><img style={footerImg} src={Email}/></a>
-    <a href="https://www.linkedin.com/in/kevinrhidalgo/"><img style={footerImg} src={Linkedin}/></a>
-    <a href="twitter.com"><img style={footerImg} src={Twitter}/></a>
-    </div>   
-</nav>
+<h5> Copyright © 2021. Kevin Hidalgo </h5>
 </div>
       </>
     );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,24 @@
 import React from "react"
+import "./style.css"
 
 function Nav() {
     return (
-      <div>
-        <h2>This is my Nav bar section</h2>
+      <>
+      <div className="navSection">
+      <div className="navLink">OUR STORY.</div>
+      <div className="navLink">RSVP.</div>
+      <div className="navLink">ADDRESS</div>
+      <div className="navLink">REGISTRY</div>
+      <div className="navLink">CONTACTS</div>
+      <div className="navLink">DONATIONS</div>
       </div>
+
+      <div className="navSection">
+        <h2>Heyyyyyy</h2>
+        </div>
+
+
+      </>
     );
   }
   
