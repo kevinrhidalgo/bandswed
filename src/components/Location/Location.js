f79a97d396849733687b96be9e537cad6a73c68e
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-11-17 01:28:36 -0500

    adding the location component and its features

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,13 +1,44 @@
 import React from "react"
+import { Link } from "react-router-dom";
+import img1 from "../../Images/bellaC1.jpeg"
+import img2 from "../../Images/bellaC2.jpeg"
+import img3 from "../../Images/bellaC3.gif"
+import "./styles.css"
+
 
 
 function Location() {
     return (
       <>
-<h1>Hello this is the page for the location</h1>
+      <div className="imageSec">
 
+<div className="imageSec1">
+<img src={img2}/>
+</div>
 
-      </>
+<div className="imageSec2">
+<img src={img3}/>
+</div>
+
+<div className="imageSec3">
+<img src={img1}/>
+</div>
+
+    </div>
+
+<h1>15920 County Rd 455, Montverde, FL 34756</h1>
+
+<div className="actionBtn">
+<div className="previous">
+<Link className="btn" to="/Aboutme"><h2> prev </h2></Link>
+</div>
+
+<div className="next">
+<Link className="btn" to="/"><h2> Home </h2></Link>
+</div>
+
+</div>
+    </>
     );
   }
   
